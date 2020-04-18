e4409788ab352556c5c1d311722e95ac397c7101
Jake <36495719+JakeC007@users.noreply.github.com>
2020-04-18 16:36:51 -0400

    Update list_to_JSON_Format.py

--- list_to_JSON_Format.py
+++ list_to_JSON_Format.py
@@ -4,37 +4,26 @@ This script does a quick and dirty job of taking a clean input of words--one wor
 per line-- and creating a string that is compatible with the Tracery JSON format.
 format of RHS: ["word1", "word2", "word3"]
 """
-
 def main():
+  inputF = ''
+  raw_inputF = input("Enter your .txt filename: ")
+  inputF = raw_inputF + '.txt' 
+  
   #Set up strings with the open bracket
-  outputN = "["
-  outputV = "["
-  outputAdv = "["
+  outputLST = "["
 
-  #open files with word lists
-  fN = open("inputN.txt", "r")
-  fV = open("inputV.txt", "r")
-  fAdv = open("inputAdv.txt", "r")
+  #open file with word lists
+  f = open(inputF, "r")
 
   #loop through the files, adding the nesscary accoutrements for formatting
-  for line in fN:
+  for line in f:
     reformat = "\"" + line.strip('\n') + "\", "
-    outputN+= reformat
-
-  for line in fV:
-    reformat = "\"" + line.strip('\n') + "\", "
-    outputV+= reformat
-
-  for line in fAdv:
-    reformat = "\"" + line.strip('\n') + "\", "
-    outputAdv+= reformat
+    outputLST+= reformat
 
   #Hacky way to get the close bracket on
-  indexN = len(outputN)-2
-  indexV = len(outputV)-2
-  indexAdv = len(outputAdv)-2
+  index = len(outputLST)-2
   
   #print desired output and copy from terminal
-  print(outputN[0:indexN] + "]\n")
-  print(outputV[0:indexV] + "]\n")
-  print(outputAdv[0:indexAdv] + "]\n")
+  print(outputLST[0:index] + "]\n")
+
+main()
